23f90337f5e04819fa3235b64162db3125d3bdc6
Obez99 <giuliogx@live.it>
2022-01-13 14:58:40 +0100

    Removes unused tag, adds edit comic form view, adds validation in ComicsController

--- resources/views/home/edit_comic.blade.php
+++ resources/views/home/edit_comic.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 
-@section('title', "DC Comics - Aggiungi fumetto")
+@section('title', 'Modifica fumetto')
 
 @section('content')
 <div class="container">
@@ -15,16 +15,17 @@
   </div>
   @endif
 
-  <form action="{{route("comics.store")}}" method="post" class="py-5">
+  <form action="{{route("comics.update", $comic->id)}}" method="post" class="py-5">
     @csrf
+    @method("PATCH")
 
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Titolo" name="title" required>
+      <input type="text" class="form-control" id="floatingInput" placeholder="Titolo" name="title">
       <label for="floatingInput">Titolo</label>
     </div>
 
     <div class="form-floating mb-3">
-      <textarea class="form-control" id="floatingInput" placeholder="Descrizione" style="height:200px" name="description" required></textarea>
+      <textarea class="form-control" id="floatingInput" placeholder="Descrizione" style="height:200px" name="description"></textarea>
       <label for="floatingTextarea2">Descrizione</label>
     </div>
 
@@ -39,7 +40,7 @@
     </div>
 
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Serie" name="series" required>
+      <input type="text" class="form-control" id="floatingInput" placeholder="Serie" name="series">
       <label for="floatingInput">Serie</label>
     </div>
 
@@ -49,7 +50,7 @@
     </div>
 
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Categoria" name="type" required>
+      <input type="text" class="form-control" id="floatingInput" placeholder="Categoria" name="type">
       <label for="floatingInput">Categoria</label>
     </div>
 
